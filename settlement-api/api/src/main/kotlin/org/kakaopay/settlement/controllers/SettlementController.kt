53ea803e56f01a3dccc42fb0b37bb86cb1155e91
anjeongkyun <dkswjdrbs12@naver.com>
2023-10-21 22:02:59 +0900

    getSettlementsForRequester API를 Controller에 등록한다

--- settlement-api/api/src/main/kotlin/org/kakaopay/settlement/controllers/SettlementController.kt
+++ settlement-api/api/src/main/kotlin/org/kakaopay/settlement/controllers/SettlementController.kt
@@ -2,19 +2,21 @@ package org.kakaopay.settlement.controllers
 
 import org.kakaopay.settlement.commands.RequestSettlementCommand
 import org.kakaopay.settlement.commands.TransferRequestedSettlementCommand
+import org.kakaopay.settlement.queries.GetSettlementForRequesterQuery
 import org.kakaopay.settlement.usecases.commands.RequestSettlementCommandExecutor
 import org.kakaopay.settlement.usecases.commands.TransferRequestedSettlementCommandExecutor
+import org.kakaopay.settlement.usecases.queries.GetSettlementsForRequesterQueryProcessor
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class SettlementController(
     private val requestedSettlementCommandExecutor: RequestSettlementCommandExecutor,
-    private val transferRequestedSettlementCommandExecutor: TransferRequestedSettlementCommandExecutor
+    private val transferRequestedSettlementCommandExecutor: TransferRequestedSettlementCommandExecutor,
+    private val getSettlementsForRequesterQueryProcessor: GetSettlementsForRequesterQueryProcessor
 ) {
 
-    @PostMapping("/settlements/commands/request-settlement/execute")
+    @PostMapping("/settlements/commands/request-settlement")
     fun requestSettlement(
-        @PathVariable("tenantId") tenantId: String,
         @RequestBody command: RequestSettlementCommand,
         @RequestHeader(value = "X-USER-ID", required = true) userId: String,
     ) {
@@ -23,14 +25,24 @@ class SettlementController(
         )
     }
 
-    @PostMapping("/settlements/commands/transfer-requested-settlement/execute")
+    @PostMapping("/settlements/commands/transfer-requested-settlement")
     fun transferRequestedSettlement(
-        @PathVariable("tenantId") tenantId: String,
         @RequestBody command: TransferRequestedSettlementCommand,
         @RequestHeader(value = "X-USER-ID", required = true) userId: String,
     ) {
         transferRequestedSettlementCommandExecutor.execute(
             command.copy(userId = userId)
+        )
+    }
+
+    @PostMapping("/settlements/quries/get-settlements-for-requester")
+    fun getSettlementsForRequester(
+        @RequestHeader(value = "X-USER-ID", required = true) userId: String,
+    ) {
+        getSettlementsForRequesterQueryProcessor.process(
+            query = GetSettlementForRequesterQuery(
+                requesterId = userId
+            )
         )
     }
 }
