19e1949a79afa89618e702d654f6f64954ddf0d5
anjeongkyun <dkswjdrbs12@naver.com>
2023-10-23 00:06:34 +0900

    UserGateway의 existsUsers의 반환값을 명시한다

--- settlement-api/gateway/src/main/kotlin/org/kakaopay/settlement/UserGatewayImpl.kt
+++ settlement-api/gateway/src/main/kotlin/org/kakaopay/settlement/UserGatewayImpl.kt
@@ -4,6 +4,6 @@ import org.kakaopay.settlement.gateways.UserGateway
 
 class UserGatewayImpl : UserGateway {
     override fun existsUsers(userIds: List<String>): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 }
