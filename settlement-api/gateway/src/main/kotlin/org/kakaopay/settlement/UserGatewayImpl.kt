7cea0a3de066744ceb673f1118f09338a26c9921
anjeongkyun <dkswjdrbs12@naver.com>
2023-10-22 22:14:36 +0900

    UserGateway의 구현체를 정의한다

---
+++ settlement-api/gateway/src/main/kotlin/org/kakaopay/settlement/UserGatewayImpl.kt
@@ -0,0 +1,9 @@
+package org.kakaopay.settlement
+
+import org.kakaopay.settlement.gateways.UserGateway
+
+class UserGatewayImpl : UserGateway {
+    override fun existsUsers(userIds: List<String>): Boolean {
+        TODO("Not yet implemented")
+    }
+}
